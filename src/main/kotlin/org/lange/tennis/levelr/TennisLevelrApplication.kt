c946c015584eb655da688c80f239607e6d4217ce
Lange <cyber_grouch_lange@yahoo.ie>
2026-06-01 15:39:13 +0800

    chore: initialize Spring Boot project
    Initial project setup with Kotlin, Gradle, and basic Spring Boot configuration.

---
+++ src/main/kotlin/org/lange/tennis/levelr/TennisLevelrApplication.kt
@@ -0,0 +1,11 @@
+package org.lange.tennis.levelr
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TennisLevelrApplication
+
+fun main(args: Array<String>) {
+    runApplication<TennisLevelrApplication>(*args)
+}
---
+++ src/test/kotlin/org/lange/tennis/levelr/TennisLevelrApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.lange.tennis.levelr
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TennisLevelrApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
